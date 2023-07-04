a43d77f219bf5759440e1552b77dcace965f1ab9
Agnieszka Rabiej <arabiej94@gmail.com>
2023-07-04 13:05:11 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document_1024px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Entire_document_1024px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Entire_document_1024px.png",
         "selector": "document",
         "fileName": "Entire_document_1024px.png",
         "label": "Entire document",
@@ -20,7 +20,9 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.029296875,
+          "misMatchPercentage": "0.03",
+          "analysisTime": 62
         }
       },
       "status": "pass"
@@ -28,7 +30,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document_720px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Entire_document_720px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Entire_document_720px.png",
         "selector": "document",
         "fileName": "Entire_document_720px.png",
         "label": "Entire document",
@@ -44,7 +46,9 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.04166666666666667,
+          "misMatchPercentage": "0.04",
+          "analysisTime": 79
         }
       },
       "status": "pass"
@@ -52,7 +56,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_1024px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Big_search_bar_data-qa_big_1024px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Big_search_bar_data-qa_big_1024px.png",
         "selector": "[data-qa=\"big\"]",
         "fileName": "Big_search_bar_data-qa_big_1024px.png",
         "label": "Big search bar data-qa_big",
@@ -68,7 +72,9 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.03968253968253968,
+          "misMatchPercentage": "0.04",
+          "analysisTime": 67
         }
       },
       "status": "pass"
@@ -76,7 +82,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_720px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Big_search_bar_data-qa_big_720px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Big_search_bar_data-qa_big_720px.png",
         "selector": "[data-qa=\"big\"]",
         "fileName": "Big_search_bar_data-qa_big_720px.png",
         "label": "Big search bar data-qa_big",
@@ -92,7 +98,9 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.056818181818181816,
+          "misMatchPercentage": "0.06",
+          "analysisTime": 54
         }
       },
       "status": "pass"
@@ -100,7 +108,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_1024px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Small_search_bar_data-qa_small_1024px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Small_search_bar_data-qa_small_1024px.png",
         "selector": "[data-qa=\"small\"]",
         "fileName": "Small_search_bar_data-qa_small_1024px.png",
         "label": "Small search bar data-qa_small",
@@ -116,7 +124,35 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.0472411186696901,
+          "misMatchPercentage": "0.05",
+          "analysisTime": 65
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_1024px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Small_search_bar_data-qa_small_1024px.png",
+        "selector": "[data-qa=\"small\"].__n1",
+        "fileName": "Small_search_bar_data-qa_small_1024px.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.0472411186696901,
+          "misMatchPercentage": "0.05",
+          "analysisTime": 64
         }
       },
       "status": "pass"
@@ -124,7 +160,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_720px.png",
-        "test": "..\\bitmaps_test\\20230630-162021\\Small_search_bar_data-qa_small_720px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Small_search_bar_data-qa_small_720px.png",
         "selector": "[data-qa=\"small\"]",
         "fileName": "Small_search_bar_data-qa_small_720px.png",
         "label": "Small search bar data-qa_small",
@@ -140,7 +176,35 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
+          "rawMisMatchPercentage": 0.06764069264069264,
+          "misMatchPercentage": "0.07",
+          "analysisTime": 39
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_720px.png",
+        "test": "..\\bitmaps_test\\20230703-185745\\Small_search_bar_data-qa_small_720px.png",
+        "selector": "[data-qa=\"small\"].__n1",
+        "fileName": "Small_search_bar_data-qa_small_720px.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720px",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.06764069264069264,
+          "misMatchPercentage": "0.07",
+          "analysisTime": 36
         }
       },
       "status": "pass"
